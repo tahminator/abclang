21a3c70a24337f12480e875e681e16922203bc78
Tahmid Ahmed <tahmid@tahmid.io>
2026-07-13 02:19:35 -0400

    add min(a,b) and max(a,b) as a builtin

--- src/eval/builtins.rs
+++ src/eval/builtins.rs
@@ -7,6 +7,14 @@ pub static BUILTINS: phf::Map<&'static str, BuiltInFunctionObject> = phf_map! {
         function: len,
         function_name: "len",
     },
+    "max" => BuiltInFunctionObject {
+        function: max,
+        function_name: "max",
+    },
+    "min" => BuiltInFunctionObject {
+        function: min,
+        function_name: "min",
+    }
 };
 
 fn len(args: &[Object]) -> Result<Object, ErrorObject> {
@@ -19,6 +27,42 @@ fn len(args: &[Object]) -> Result<Object, ErrorObject> {
         }),
         _ => Err(ErrorObject {
             msg: format!("wrong number of arguments. got={}, want=1", args.len()),
+        }),
+    }
+}
+
+fn max(args: &[Object]) -> Result<Object, ErrorObject> {
+    match args {
+        [Object::Integer(l), Object::Integer(r)] => Ok(Object::Integer(IntegerObject {
+            value: std::cmp::max(l.value, r.value),
+        })),
+        [l, r] => Err(ErrorObject {
+            msg: format!(
+                "arguments to `max` not supported, got {} and {}",
+                l.typ(),
+                r.typ()
+            ),
+        }),
+        _ => Err(ErrorObject {
+            msg: format!("wrong number of arguments. got={}, want=2", args.len()),
+        }),
+    }
+}
+
+fn min(args: &[Object]) -> Result<Object, ErrorObject> {
+    match args {
+        [Object::Integer(l), Object::Integer(r)] => Ok(Object::Integer(IntegerObject {
+            value: std::cmp::min(l.value, r.value),
+        })),
+        [l, r] => Err(ErrorObject {
+            msg: format!(
+                "arguments to `min` not supported, got {} and {}",
+                l.typ(),
+                r.typ()
+            ),
+        }),
+        _ => Err(ErrorObject {
+            msg: format!("wrong number of arguments. got={}, want=2", args.len()),
         }),
     }
 }
