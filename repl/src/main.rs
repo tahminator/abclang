5cfae2e6e9d2d97afdc4edbe506cb313d5e7684a
Tahmid Ahmed <tahmid@tahmid.io>
2026-07-27 00:29:42 -0400

    remove client.rs files

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,9 +1,11 @@
-mod client;
 mod error;
+mod repl;
+
+use crate::repl::Repl;
 
 fn main() {
     println!("welcome to abclang!");
     println!();
     let mut rl = rustyline::DefaultEditor::new().expect("rustyline failed to be initialized");
-    client::Repl::new().start(&mut rl).unwrap()
+    Repl::new().start(&mut rl).unwrap()
 }
